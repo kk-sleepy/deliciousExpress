0144a9f79f248d6e0c1aee0ccc96202a9d4ed961
kk-sleepy <1610896646@qq.com>
2025-04-08 17:47:06 +0800

    新增菜品相关业务功能

--- deliciousExpress-server/src/main/java/com/sky/mapper/DishMapper.java
+++ deliciousExpress-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,5 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -8,10 +12,9 @@ public interface DishMapper {
 
     /**
      * 根据分类id查询菜品数量
-     * @param categoryId
-     * @return
      */
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
-
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dis);
 }
