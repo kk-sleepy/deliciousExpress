9bfbc1747a52f268519f3d59791cdaf8cf6cff56
kk-sleepy <1610896646@qq.com>
2025-04-10 23:07:33 +0800

    C端订单支付业务开发

--- deliciousExpress-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ deliciousExpress-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -4,6 +4,9 @@ import com.sky.entity.AddressBook;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
 
 @Mapper
 public interface OrderMapper {
@@ -15,4 +18,20 @@ public interface OrderMapper {
 
     @Select("SELECT * FROM address_book WHERE id = #{addressBookId}")
     AddressBook getById(Long addressBookId);
+
+    /**
+     * 根据订单号查询订单
+     * @param orderNumber
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+
+    @Update("UPDATE orders SET pay_status = #{orderPaidStatus}, status = #{orderStatus}, checkout_time = #{checkoutTime} WHERE number = #{orderNumber}")
+    void updateStatus(String orderNumber, Integer orderPaidStatus, Integer orderStatus, LocalDateTime checkoutTime);
 }
